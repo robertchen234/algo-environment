2851f5e8c6572f7acd4fd3250fc871c0d04e1f17
rob <robertchen234@gmail.com>
2019-04-02 14:50:34 -0400

    simple and it works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+// PROBLEM
+// Input: "abcde"
+// Output: "edcba"
+// Explanation: reverse the string.
+
+const solution = input => {
+  const revString = [];
+
+  for (let x of input) {
+    revString.unshift(x);
+  }
+
+  return revString;
+};
+
+const input1 = "abcde";
+const input2 = "apple";
+const input3 = "orange";
+
+// yarn start or npm start
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="input">
+          <h1>Input1: {input1}</h1>
+          <h1>Input2: {input2}</h1>
+          <h1>Input3: {input3}</h1>
+        </div>
+        <div className="output">
+          <h1>Output1: {solution(input1)}</h1>
+          <h1>Output2: {solution(input2)}</h1>
+          <h1>Output3: {solution(input3)}</h1>
+        </div>
       </div>
     );
   }
